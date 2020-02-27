0edc461a379936a6579bfd6b7ee80be3e0394508
superjoy0502 <superjoy0502@gmail.com>
2020-02-27 16:14:34 -0700

    JAVA METHOD 수업 시작

---
+++ ProjectMethod/src/WhyMethod.java
@@ -0,0 +1,18 @@
+public class WhyMethod {
+    public static void printTwoTimesA(){
+        System.out.println("-");
+        System.out.println("A");
+        System.out.println("A");
+    }
+
+    public static void main(String[] args) {
+
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+
+    }
+}
